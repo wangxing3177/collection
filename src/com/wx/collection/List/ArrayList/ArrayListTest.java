9183f819964bd0102d39050a3790c949d0aad6bd
wangxing <wangxing3177@163.com>
2022-04-26 07:44:01 +0800

    添加collection类图和常用方法

--- src/com/wx/collection/List/ArrayList/ArrayListTest.java
+++ src/com/wx/collection/List/ArrayList/ArrayListTest.java
@@ -1,4 +1,13 @@
 package com.wx.collection.List.ArrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListTest {
+
+    public static void main(String[] args) {
+        List objects = new ArrayList<>();
+        objects.add(1);
+        objects.add("s");
+    }
 }

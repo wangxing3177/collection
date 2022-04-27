26d5a18dc09518f7d805f66b0d67f814715edfac
wangxing <wangxing3177@163.com>
2022-04-27 11:06:06 +0800

    测试循环和arraylist总结

--- src/com/wx/collection/List/ArrayList/ArrayListTest.java
+++ src/com/wx/collection/List/ArrayList/ArrayListTest.java
@@ -1,6 +1,7 @@
 package com.wx.collection.List.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListTest {
@@ -9,6 +10,24 @@ public class ArrayListTest {
         List objects = new ArrayList<>();
         objects.add(1);
         objects.add("s");
+        objects.add("ws");
+        objects.add("hah");
+
+        //普通循环
+        for (int i = 0; i < objects.size(); i++) {
+            System.out.println(objects.get(i));
+        }
+        System.out.println("====================================");
+        //增强for循环
+        for (Object o: objects) {
+            System.out.println(o);
+        }
+        System.out.println("====================================");
+        //迭代器循环
+        Iterator iterator = objects.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
 
 
     }

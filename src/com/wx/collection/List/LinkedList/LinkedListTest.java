567d1c7826fb26ab61b4462b3f47f7e7d7102fe8
wangxing <wangxing3177@163.com>
2022-04-28 09:15:08 +0800

    list集合总结

--- src/com/wx/collection/List/LinkedList/LinkedListTest.java
+++ src/com/wx/collection/List/LinkedList/LinkedListTest.java
@@ -1,4 +1,19 @@
 package com.wx.collection.List.LinkedList;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListTest {
+
+    public static void main(String[] args) {
+        List<Object> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(1);
+
+        Iterator iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
--- src/com/wx/collection/List/Vector/VectorTest.java
+++ src/com/wx/collection/List/Vector/VectorTest.java
@@ -1,4 +1,12 @@
 package com.wx.collection.List.Vector;
 
+import java.util.List;
+import java.util.Vector;
+
 public class VectorTest {
+
+    public static void main(String[] args) {
+        List<Object> vector = new Vector<>();
+        vector.add(1);
+    }
 }
